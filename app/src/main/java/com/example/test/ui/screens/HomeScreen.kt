d8a7e4652f78674a9e1d3ba86f0a558835760caf
Git <steven.kariuki9@outlook.com>
2023-06-29 12:21:21 +0300

    new commit

--- app/src/main/java/com/example/test/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/test/ui/screens/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.test.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -18,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -50,8 +56,10 @@ private fun CustomizeThisCard(uiModel: UIModel) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .clip(RoundedCornerShape(16.dp)),
+            .padding(20.dp)
+            .background(Color.Blue)
+            .border(10.dp, Color.Blue)
+            .clip(RoundedCornerShape(80.dp)),
 
     ) {
         Row( //TODO 1
@@ -64,6 +72,9 @@ private fun CustomizeThisCard(uiModel: UIModel) {
             Image(
                 modifier = Modifier
                     .size(80.dp, 80.dp)
+                    .padding(2.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .border(2.dp, Color.Blue)
                     .clip(RoundedCornerShape(16.dp)),
                 painter = image,
                 alignment = Alignment.CenterStart,
@@ -74,15 +85,18 @@ private fun CustomizeThisCard(uiModel: UIModel) {
                 modifier = Modifier
                     .padding(start = 12.dp)
             ) {
-                Text(text = uiModel.name, style = MaterialTheme.typography.titleMedium) // TODO 4 learn about Styling text https://www.youtube.com/watch?v=nm_LNJWHi9A
+                Text(text = uiModel.name, modifier = Modifier.clickable {
+
+                } ) // TODO 4 learn about Styling text https://www.youtube.com/watch?v=nm_LNJWHi9A
             }
+
         }
     }
 }
 
 val basketBallItemsList = listOf(
     UIModel(
-        name = "Shoe 1",
+        name = "View",
         image = R.drawable.vituofly
     ),
     UIModel(
